2adafb357649f36d1b9251870fe905635a6b592f
Abhinavzenith12 <153698775+Abhinavzenith12@users.noreply.github.com>
2023-12-18 15:41:54 +0000

    Operations in Js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,5 +29,28 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// Operations
+
+let value = 3
+let negativeValue = -value
+// console.log(negativeValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+
+let str1 = "Abhinav"
+let str2 = "Rajan"
+
+let str3 = str1 + str2;
+//console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
